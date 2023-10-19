3f346c16b5b47e12b5decfccb3249075bf24f315
Pedro <pedro.dpaula67@gmail.com>
2023-10-19 16:32:51 -0300

    selecionando as partes do menu

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+"use strict"
+
+// Selecionando o elemento que acionará o menu
+const botaomenu = document.querySelector("nav h2");
+
+// Selecionando a lista/menu
+const menu = document.querySelector(".menu");
+
+// Selecionando o link que está dentro do nav h2
+const linkmenu = botaomenu.querySelector("a");
